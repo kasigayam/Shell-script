8192bd7bae4f68c992ba4215863cff587e689ae3
kasiswer <kasiswer@gmail.com>
2024-02-24 12:18:51 +0530

    variables commit

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Kasi: Hello Ram, Good Morning"
+echo "Ram: Hello Kasi, Good Morning"
+echo "Kasi: Where are you going to hoildays in summer"
+echo "Ram: I am going to my granny home in summer, what about you"
+echo "Kasi: I am busy with my dad's sweet shop business to take in market new products."
